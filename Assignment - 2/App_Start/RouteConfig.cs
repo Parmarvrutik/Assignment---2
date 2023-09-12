6dec23d3152589ada6ffe029c5eaffb24eb415cd
LENOVO <LENOVO@LAPTOP-CNKVN11E>
2023-09-12 16:29:01 +0530

    Add project files.

---
+++ Assignment - 2/App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Assignment___2
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Student", action = "Register", id = UrlParameter.Optional }
+            );
+
+            // Route for the search action
+            routes.MapRoute(
+                name: "Search",
+                url: "{controller}/Index/{selectedDept}", // Adjust the URL pattern as needed
+                defaults: new { controller = "Student", action = "Index", selectedDept = UrlParameter.Optional }
+            );
+        }
+    }
+}
